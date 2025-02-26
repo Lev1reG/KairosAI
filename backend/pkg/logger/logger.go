d14eba1e921ea93828fdaca321a338344f74d980
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-02-26 16:47:59 +0700

    feat: logging to file & console

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,26 +1,50 @@
 package logger
 
 import (
+	"os"
+	"time"
+
 	"github.com/Lev1reG/kairosai-backend/config"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Log *zap.Logger
 
 func InitLogger(cfg *config.Config) {
-	var logger *zap.Logger
-	var err error
-
+	var logLevel zapcore.Level
 	if cfg.APP_ENV == "production" {
-		logger, err = zap.NewProduction()
+		logLevel = zapcore.InfoLevel
 	} else {
-		logger, err = zap.NewDevelopment()
+		logLevel = zapcore.DebugLevel
 	}
 
+	logFile := "logs/app.log"
+	_ = os.Mkdir("logs", 0755)
+
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		panic("Failed to initialize logger: " + err.Error())
+		panic("Failed to open log file: " + err.Error())
 	}
 
-	Log = logger
-	defer Log.Sync()
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
+
+	fileCore := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.AddSync(file),
+		logLevel,
+	)
+
+  consoleCore := zapcore.NewCore(
+    zapcore.NewConsoleEncoder(encoderConfig),  
+    zapcore.AddSync(os.Stdout),
+    logLevel,
+  )
+
+  core := zapcore.NewTee(fileCore, consoleCore)
+
+  Log = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+  defer Log.Sync()
 }
